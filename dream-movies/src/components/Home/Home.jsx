8a90d4809024cceb6811a1c8020ba38fe990de37
Liam Marega <liammarega85@gmail.com>
2022-02-08 17:25:17 -0300

    demo1
    asd
    
    Co-Authored-By: Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>

--- dream-movies/src/components/Home/Home.jsx
+++ dream-movies/src/components/Home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 // import {allMovies} from "../store/reducer";
 
+<<<<<<< Updated upstream
 export default function Home(){
+=======
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Container fluid>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+>>>>>>> Stashed changes
 
   return(
     <div>blaaa</div>
