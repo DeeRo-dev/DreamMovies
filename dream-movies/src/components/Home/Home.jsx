5c60ffb0ce85e089c3c2c31eb61d27fc1a1181e2
Liam Marega <liammarega85@gmail.com>
2022-02-08 17:19:36 -0300

    devv

---
+++ dream-movies/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "../index";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Home;
---
+++ dream-movies/src/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as NavBar } from './NavBar/NavBar.jsx'
+export { default as Movie } from './Movie/Movie.jsx'
+
