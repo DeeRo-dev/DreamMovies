a16464b590532403d5e8a26f5cd0d798bbef9c21
Liam Marega <liammarega85@gmail.com>
2022-02-09 18:53:11 -0300

    maquetado y componente movies

--- dream-movies/src/components/Movie/Movie.jsx
+++ dream-movies/src/components/Movie/Movie.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import { NavBar } from "../index";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Movie() {
-  return <div></div>;
+  return (
+    <div>
+       <NavBar />
+      <Container fluid className="Movie__Container">
+
+      </Container>
+    </div>
+  );
 }
 
 export default Movie;
--- dream-movies/src/components/index.jsx
+++ dream-movies/src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as NavBar } from './NavBar/NavBar.jsx'
 export { default as Movie } from './Movie/Movie.jsx'
+export { default as Home } from './Home/Home.jsx'
 
