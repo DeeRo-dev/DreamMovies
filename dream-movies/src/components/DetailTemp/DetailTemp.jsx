36551fe4fccc82ac7cdead9511c2d63e9887ecac
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-02-15 17:31:01 -0300

    link en la cards

--- dream-movies/src/components/DetailTemp/DetailTemp.jsx
+++ dream-movies/src/components/DetailTemp/DetailTemp.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {getTempSimp} from "../../store/actions"
 import CardTempSimp from "../CardTempSimp/CardTempSimp";
 import { Container, Row, Col } from "react-bootstrap";
-
+import {Link} from 'react-router-dom'
 export default function DetailTemp(){
 
   const dispatch = useDispatch()
@@ -33,12 +33,14 @@ export default function DetailTemp(){
                  
                   return(
                     <div key={i.id}  className="DetailTemp__Cards">
+                  <Link to={`/movie/${i.season_number}`}> 
                     <CardTempSimp
                       
                        img={i.poster_path}
                        estreno={i.air_date}
                        temp={i.name}
-                    />
+                    /> 
+                     </Link>
                     </div>
                   )
                 }
