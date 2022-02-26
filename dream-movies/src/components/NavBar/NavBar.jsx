8dd608c87f2d7e9088509cc8dafff0bd2eb06f6a
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-02-25 22:12:48 -0300

    arreglo de carrusel y nav

--- dream-movies/src/components/NavBar/NavBar.jsx
+++ dream-movies/src/components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ import { Container, Row, Col, Carousel, Navbar, Nav, NavDropdown, Form, FormCont
 
 function NavBar() {
   return (
-    <div>
+    <div >
       <Container fluid className="NavBar__ContainerNav">
-        <Navbar bg="primary" variant="dark" fixed="top" expand="lg">
-          <Container fluid>
-            <Navbar.Brand href="http://localhost:3000/">Dream Movies</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar  className="NavBar__Nav"   fixed="top" expand="lg">
+          <Container fluid >
+            <Navbar.Brand href="http://localhost:3000/" className="NavBar__TitleDream">Dream Movies</Navbar.Brand>
+            {/* <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
                 className="me-auto my-2 my-lg-0"
@@ -42,44 +42,42 @@ function NavBar() {
                 />
                 <Button variant="outline-success">Search</Button>
               </Form>
-            </Navbar.Collapse> 
+            </Navbar.Collapse>  */}
           </Container>
         </Navbar>
     <Carousel>
           <Carousel.Item interval={4000}>
             <img
-             
-              src="http://www.emprendemania.com/wp-content/uploads/2013/08/thesimpsons.jpg"
+               className="NavBar__Img"
+              src="https://media.istockphoto.com/photos/video-movie-concept-movie-background-picture-id1170514267?b=1&k=20&m=1170514267&s=170667a&w=0&h=Z4_cBKgB5AbLxR53xHrQN7IIE-fIWAww3O12qZY6yQA="
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>PRIMER FOTO</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Cinema</h3>
+              <p>Podras ver las peliculas que mas te guste!</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={4000}>
             <img
-              
-              src="https://laquilombo.files.wordpress.com/2014/08/los-simpsons-banner.jpg"
+              src="https://i1.wp.com/www.senpai.com.mx/wp-content/uploads/2021/10/Hora-de-Aventura-es-recreado-al-estilo-del-anime-con-estas-increibles-ilustraciones.jpg?fit=1280%2C720&ssl=1"
               alt="Second slide"
+              className="NavBar__Img"
             />
          
             <Carousel.Caption>
-              <h3>SEGUNDA FOTO</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3>Hora de Aventura</h3>
+              <p>Podras ver la serie completa</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={4000}>
             <img
-              
-              src="https://elvortex.com/wp-content/uploads/2014/10/the-simpsons-banner-db96ea.jpg"
+                className="NavBar__Img"
+              src="https://www.milenio.com/uploads/media/2020/06/12/doblaje-mexicano-serie-animada-contribuido.jpg"
               alt="Third slide"
             />
             <Carousel.Caption>
-              <h3>TERCER FOTO</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
+              <h3>Los Simpsons</h3>
+              <p>Podras ver la serie completa</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel> 
