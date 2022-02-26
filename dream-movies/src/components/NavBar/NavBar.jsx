e49fb2133773047cbb97ae451b0e5a80df584356
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-02-25 23:22:29 -0300

    cambio de imagenes en el carrusel

--- dream-movies/src/components/NavBar/NavBar.jsx
+++ dream-movies/src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
         <Navbar  className="NavBar__Nav"   fixed="top" expand="lg">
           <Container fluid >
             <Navbar.Brand href="http://localhost:3000/" className="NavBar__TitleDream">Dream Movies</Navbar.Brand>
-            {/* <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
               <Nav
                 className="me-auto my-2 my-lg-0"
@@ -42,14 +42,14 @@ function NavBar() {
                 />
                 <Button variant="outline-success">Search</Button>
               </Form>
-            </Navbar.Collapse>  */}
+            </Navbar.Collapse> 
           </Container>
         </Navbar>
     <Carousel>
           <Carousel.Item interval={4000}>
             <img
                className="NavBar__Img"
-              src="https://media.istockphoto.com/photos/video-movie-concept-movie-background-picture-id1170514267?b=1&k=20&m=1170514267&s=170667a&w=0&h=Z4_cBKgB5AbLxR53xHrQN7IIE-fIWAww3O12qZY6yQA="
+              src="https://asicsulb.org/corporate/images/connect/beachfront/2018-03/Movies-on-the-House.jpg"
               alt="First slide"
             />
             <Carousel.Caption>
@@ -59,7 +59,7 @@ function NavBar() {
           </Carousel.Item>
           <Carousel.Item interval={4000}>
             <img
-              src="https://i1.wp.com/www.senpai.com.mx/wp-content/uploads/2021/10/Hora-de-Aventura-es-recreado-al-estilo-del-anime-con-estas-increibles-ilustraciones.jpg?fit=1280%2C720&ssl=1"
+              src="http://i.imgur.com/4bucF8b.png"
               alt="Second slide"
               className="NavBar__Img"
             />
