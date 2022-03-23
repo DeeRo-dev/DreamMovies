04b6db0f9390ab654708994cdd932358b8cbdb64
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-03-22 21:21:26 -0300

    footer semi terminado

--- dream-movies/src/components/Footer/Footer.jsx
+++ dream-movies/src/components/Footer/Footer.jsx
@@ -11,43 +11,53 @@ export default function Footer(){
     <Container fluid className="Footer__Content">
       <Row>
         <Col  className="Footer__prueba">
-        <div>
-          <h1>Dream Movies</h1>
-           <h4>Derechos reservados</h4>
-           <h4>copy rigth</h4>
-        </div>
+        <Container className="Footer__TextDerechos">
+          <h1 className="Footer__Titleh1">™DreamMovies</h1>
+          <hr />
+          <div className="Footer__Span">
+            <span className="Footer__Span">®Derechos reservados</span>
+           <span className="Footer__Span">©Copyright</span>
+          </div>
+         
+        </Container>
             
         </Col>
         <Col md={5} className="Footer__prueba2">
-      
-        <h2>Desarrolladores</h2>
-        <Container>
-           <div>
-              <h4>Derek Cabrera</h4>
+      <Container className="Footer__TitleDevs">
+        <h1 className="Footer__Titleh1">Desarrolladores</h1>
+       <hr />
+      </Container>
+        <Container className="Footer__ContentDevs">
+           <div className="Footer__NombresDevs">
+          
+              <a  target={'blanck'} href="https://derek-cabrera.vercel.app/"><h4 className="Footer__Titleh4">Derek Cabrera</h4></a>
+             
               <ul>
-                 <li><FontAwesomeIcon icon={faLinkedin} /></li>
-                  <li><FontAwesomeIcon icon={faGithub} /></li>
+              <a  target={'blanck'} href="https://www.linkedin.com/in/derek-cabrera-fullstack/"> <li  className="Footer__Li"><FontAwesomeIcon icon={faLinkedin} /></li></a>
+              <a  target={'blanck'} href="https://github.com/DeeRo-dev"> <li className="Footer__Li" ><FontAwesomeIcon icon={faGithub} /></li></a>
               </ul>
             </div> 
-            <div>
+            <div className="Footer__NombresDevs">
+            
+            <a  target={'blanck'} href="https://portfolio-liammarega.netlify.app/"><h4 className="Footer__Titleh4">Liam Marega</h4></a>
               
-              <h4>Liam Marega</h4>
-              <ul>
-              <li><FontAwesomeIcon icon={faLinkedin} /></li>
-                  <li><FontAwesomeIcon icon={faGithub} /></li>
+              <ul >
+              <a  target={'blanck'} href="https://www.linkedin.com/in/liam-marega/"> <li className="Footer__Li"><FontAwesomeIcon icon={faLinkedin} /></li></a>
+              <a  target={'blanck'} href="https://github.com/LiamMarega"> <li className="Footer__Li"><FontAwesomeIcon icon={faGithub} /></li></a>
               </ul>
             </div> 
         </Container>
     
         </Col>
         <Col className="Footer__prueba3">
-        <div>
-              <h1> contacto </h1>
+        <Container className="Footer__TextContact">
+              <h1 className="Footer__Titleh1"> Contacto </h1>
+              <hr />
               <ul>
-                  <li>DreamMovies@dream.com</li>
+                  <li className="Footer__Email">DreamMovies@dream.com</li>
                
               </ul>
-           </div>
+           </Container>
         </Col>
       </Row>
     </Container>
